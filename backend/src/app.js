0048b38083b89e8d10f8bf92cf689c483c5e7144
David <davidacuna@Davids-MBP.home>
2021-04-28 19:30:37 +0200

    upload database

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ const cors =require('cors');
 const app = express();
 const morgan = require('morgan')
 
-//require('./database')
+require('./database')
 app.set('port', process.env.PORT || 4000)
 app.use(cors())
 app.use(express.json())
